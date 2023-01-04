3800714d16a127a0568dde2ea111802ba0b0bcb6
leandrovsk <contas.center@gmail.com>
2023-01-04 14:41:34 -0300

    feat: add login and register pages

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import LoginForm from "../../components/LoginForm";
+
+import { StyledLoginPage } from "./styles";
+
+const Login = () => {
+  return (
+    <StyledLoginPage>
+      <LoginForm />
+    </StyledLoginPage>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledRegisterPage = styled.main`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    gap: 62px;
+`
