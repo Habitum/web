fc3c559598282b157bd83201177390964c521f62
Gabriela <gabrielagarciafontoura@gmail.com>
2023-01-04 16:44:31 -0300

    feat: BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 
 import App from "./App";
@@ -7,8 +8,10 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
