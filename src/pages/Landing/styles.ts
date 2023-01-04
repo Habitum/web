ad06e83cae9c2078dac4adae57b595c429e65a3b
Gabriela <gabrielagarciafontoura@gmail.com>
2023-01-04 16:11:12 -0300

    feat: landing page

---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledLandingPage = styled.main`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+   
+`
