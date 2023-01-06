b043a13e1b6872ee4e4616df0e2309a88be08064
Maikol Santos <maikol.lourencon@gmail.com>
2023-01-06 12:17:17 -0300

    feat: create hero section

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import { FaUserAlt } from "react-icons/fa";
 
 import Header from "../../components/Header";
-import { StyledLink } from "../../styles/StyledLink";
 
-import { StyledLandingPage } from "./styles";
+import { StyledLink } from "../../styles/StyledLink";
+import { StyledHero } from "./styles";
+
+import imageHero from '../../assets/images/image-hero.svg' 
 
 const LandingPage = () => {
   return (
-    <StyledLandingPage>
+    <>
       <Header>
         <ul>
           <li>
@@ -24,7 +26,17 @@ const LandingPage = () => {
           </li>
         </ul>
       </Header>
-    </StyledLandingPage>
+
+      <section>
+        <StyledHero>
+          <img src={imageHero} alt="Criando Hábitos" />
+          <h2>
+            Completar <strong>tarefas</strong> e <strong>hábitos</strong> será
+            ainda mais divertido
+          </h2>
+        </StyledHero>
+      </section>
+    </>
   );
 };
 
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,9 +1,39 @@
 import styled from "styled-components";
+import { DefaultContainer } from "../../styles/container";
 
-export const StyledLandingPage = styled.main`
+export const StyledHero = styled(DefaultContainer)`
   display: flex;
-  height: 100vh;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: space-between;
+  margin-top: 10.5rem;
+
+  @media (min-width: 510px) {
+    margin-top: 8.25rem;
+  }
+
+  img {
+    display: none;
+
+    @media (min-width: 700px) {
+      width: 50%;
+      max-width: 29.625rem;
+      display: block;
+    }
+  }
+
+  h2 {
+    color: var(--color-brand-200);
+    font-size: 2.875rem;
+    text-align: center;
+    font-weight: var(--text-weight-200);
+
+    strong {
+      color: var(--color-brand-100);
+    }
+
+    @media (min-width: 700px) {
+      font-size: 3.5rem;
+      max-width: 31.25rem;
+    }
+  }
 `;
