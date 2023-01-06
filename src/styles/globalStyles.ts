c09a88c483816c33c2032434e8cbb7871e06806c
Maikol Santos <maikol.lourencon@gmail.com>
2023-01-05 22:11:14 -0300

    fix: creat container component

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -57,9 +57,3 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0 20px;
   }
 `;
-
-export const MainContainer = styled.div`
-  max-width: 1200px;
-  width: 100%;
-  margin: 0 auto;
-`;
