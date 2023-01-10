581bd9e110ac0ac3e2ad822ec42a545c64eb6753
leandrovsk <contas.center@gmail.com>
2023-01-10 11:43:44 -0300

    refactor: forms components and styles

--- src/components/Forms/LoginForm/index.tsx
+++ src/components/Forms/LoginForm/index.tsx
@@ -4,17 +4,17 @@ import { UserContext } from "../../../contexts/UserContext/UserContext";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import Button from "../../Button";
+import FormHeader from "../FormHeader";
 import Input from "../../Input";
+import Spinner from "../../Spinner";
 
 import { iLoginFormValues } from "./types";
 
 import { formSchema } from "./schema";
 
-import { StyledLoginForm } from "./styles";
-import FormHeader from "../FormHeader";
 import { StyledLink } from "../../../styles/StyledLink";
-import Button from "../../Button";
-import Spinner from "../../Spinner";
+import { StyledForm } from "../../../styles/forms";
 
 const LoginForm = () => {
   const { userLogin, globalLoading } = useContext(UserContext);
@@ -30,28 +30,26 @@ const LoginForm = () => {
   };
 
   return (
-    <>
-      <StyledLoginForm onSubmit={handleSubmit(submit)}>
-        <FormHeader name="Login" linkname="Voltar" linkto="/" />
-        <Input placeholder="Digite seu email" name="email" label="Email" type="email" register={register("email")} />
-        {errors.email && (
-          <p className="FormError">
-            <>{errors.email.message}</>
-          </p>
-        )}
-        <Input placeholder="Digite sua senha" name="password" label="Senha" type="password" register={register("password")} />
-        {errors.password && (
-          <p className="FormError">
-            <>{errors.password?.message}</>
-          </p>
-        )}
-        <Button name={globalLoading ? <Spinner /> : "Entrar"} variant={globalLoading ? "forms-disabled" : "forms"} />
-        <p className="FormQuestion">Ainda não tem conta? Crie uma conta</p>
-        <StyledLink to="/register" variant="default">
-          CRIAR CONTA
-        </StyledLink>
-      </StyledLoginForm>
-    </>
+    <StyledForm onSubmit={handleSubmit(submit)}>
+      <FormHeader name="Login" linkname="Voltar" linkto="/" />
+      <Input placeholder="Digite seu email" name="email" label="Email" type="email" register={register("email")} />
+      {errors.email && (
+        <p className="FormError">
+          <>{errors.email.message}</>
+        </p>
+      )}
+      <Input placeholder="Digite sua senha" name="password" label="Senha" type="password" register={register("password")} />
+      {errors.password && (
+        <p className="FormError">
+          <>{errors.password?.message}</>
+        </p>
+      )}
+      <Button name={globalLoading ? <Spinner /> : "Entrar"} variant={globalLoading ? "forms-disabled" : "forms"} />
+      <p className="FormQuestion">Ainda não tem conta? Crie uma conta</p>
+      <StyledLink to="/register" variant="default">
+        CRIAR CONTA
+      </StyledLink>
+    </StyledForm>
   );
 };
 
--- src/components/Forms/RegisterForm/index.tsx
+++ src/components/Forms/RegisterForm/index.tsx
@@ -1,19 +1,19 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../../contexts/UserContext/UserContext";
 
-import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-import Input from "../../Input";
+import Button from "../../Button";
 import FormHeader from "../FormHeader";
+import Input from "../../Input";
+import Spinner from "../../Spinner";
 
 import { iRegisterFormValues } from "./types";
 
 import { formSchema } from "./schema";
 
-import { StyledRegisterForm } from "./styles";
-import Button from "../../Button";
-import Spinner from "../../Spinner";
+import { StyledForm } from "../../../styles/forms";
 
 const RegisterForm = () => {
   const {
@@ -29,42 +29,40 @@ const RegisterForm = () => {
   };
 
   return (
-    <>
-      <StyledRegisterForm onSubmit={handleSubmit(submit)}>
-        <FormHeader name="Cadastro" linkname="Ir para o login" linkto="/login" />
-        <Input name="name" type="text" label="Nome" placeholder="Digite seu nome" register={register("name")} />
-        {errors.name && (
-          <p className="FormError">
-            <>{errors.name.message}</>
-          </p>
-        )}
-        <Input name="userName" type="text" label="Username" placeholder="Digite seu nome de usuário" register={register("userName")} />
-        {errors.userName && (
-          <p className="FormError">
-            <>{errors.userName.message}</>
-          </p>
-        )}
-        <Input name="email" type="email" label="Email" placeholder="Digite seu email" register={register("email")} />
-        {errors.email && (
-          <p className="FormError">
-            <>{errors.email.message}</>
-          </p>
-        )}
-        <Input name="password" type="password" label="Senha" placeholder="Senha" register={register("password")} />
-        {errors.password && (
-          <p className="FormError">
-            <>{errors.password.message}</>
-          </p>
-        )}
-        <Input name="confirmPassword" type="password" label="Confirmar Senha" placeholder="Confirmar senha" register={register("confirmPassword")} />
-        {errors.confirmPassword && (
-          <p className="FormError">
-            <>{errors.confirmPassword.message}</>
-          </p>
-        )}
-        <Button name={globalLoading ? <Spinner /> : "CRIAR CONTA"} variant={globalLoading ? "forms-disabled" : "forms"} />
-      </StyledRegisterForm>
-    </>
+    <StyledForm onSubmit={handleSubmit(submit)}>
+      <FormHeader name="Cadastro" linkname="Ir para o login" linkto="/login" />
+      <Input name="name" type="text" label="Nome" placeholder="Digite seu nome" register={register("name")} />
+      {errors.name && (
+        <p className="FormError">
+          <>{errors.name.message}</>
+        </p>
+      )}
+      <Input name="userName" type="text" label="Username" placeholder="Digite seu nome de usuário" register={register("userName")} />
+      {errors.userName && (
+        <p className="FormError">
+          <>{errors.userName.message}</>
+        </p>
+      )}
+      <Input name="email" type="email" label="Email" placeholder="Digite seu email" register={register("email")} />
+      {errors.email && (
+        <p className="FormError">
+          <>{errors.email.message}</>
+        </p>
+      )}
+      <Input name="password" type="password" label="Senha" placeholder="Senha" register={register("password")} />
+      {errors.password && (
+        <p className="FormError">
+          <>{errors.password.message}</>
+        </p>
+      )}
+      <Input name="confirmPassword" type="password" label="Confirmar Senha" placeholder="Confirmar senha" register={register("confirmPassword")} />
+      {errors.confirmPassword && (
+        <p className="FormError">
+          <>{errors.confirmPassword.message}</>
+        </p>
+      )}
+      <Button name={globalLoading ? <Spinner /> : "CRIAR CONTA"} variant={globalLoading ? "forms-disabled" : "forms"} />
+    </StyledForm>
   );
 };
 
