6eac344ff13f2ade765cf734a8c6ffc267f994a8
Luan F. Silva <luanflorencio87@gmail.com>
2023-01-11 15:36:17 -0300

    refact: Project.tsx

--- src/pages/AdditionalPages/Project/index.tsx
+++ src/pages/AdditionalPages/Project/index.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+
 import Button from "../../../components/Button";
 import Wave from "../../../components/Wave";
+
 import { StyledContainer, StyledMain, StyledSection } from "./styles";
 
 const Project = () => {
